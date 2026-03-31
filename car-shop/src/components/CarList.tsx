c46e86f838230b898558b4670b50072831664a37
lottavahe <lotta.vahe@gmail.com>
2026-03-31 14:21:05 +0300

    lisäsinpoisto funktion ja napin jota painamalla se suoritetaan

--- car-shop/src/components/CarList.tsx
+++ car-shop/src/components/CarList.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import type { CarData } from "../types";
 import { DataGrid } from "@mui/x-data-grid";
-import type { GridColDef} from "@mui/x-data-grid";
+import type { GridColDef, GridRenderCellParams} from "@mui/x-data-grid";
 function CarList() {
     const [cars, setCars] = useState<CarData[]>([]);
 
@@ -12,6 +12,17 @@ function CarList() {
         {field: "fuel", headerName: "Fuel type"},
         {field: "modelYear", headerName: "Model year"},
         {field: "price", headerName: "Price €"},
+        {field: "_links.self.href", 
+            headerName: "",
+            sortable: false,
+            filterable: false,
+            disableColumnMenu: true,
+            renderCell: (params: GridRenderCellParams) =>
+                <button onClick={() => 
+                    handleDelete(params.id as string)}> DELETE
+                </button>
+
+        }
     ]
 
     const getCars = () => {
@@ -24,14 +35,30 @@ function CarList() {
             .then(data => setCars(data._embedded.cars))
             .catch(error => console.error(error));
     }
+
+    const handleDelete = (url: string) => {
+        if (window.confirm("Are you shure you want to delete it?")) {
+            fetch(url, {
+                method: "DELETE"
+            })
+            .then(response => {
+                if (!response.ok)
+                    throw new Error ("error when deleting a car");
+                return response.json();
+            })
+            .then(() => getCars())
+            .catch(error => console.error(error))
+        }
+    }
     useEffect(() => {
         getCars();
     },[])
 
+
     
     return (
         <>
-        <div>
+        <div style={{height: 500, width: "100%"}}>
             <DataGrid
             columns={columns}
             rows={cars}
